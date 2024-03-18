9550b87998261d16a3c9ba111c4c9ee57a03a670
jonwcole <jon@jonwcole.com>
2024-03-17 20:20:59 -0400

    Update horrordle.js

--- horrordle/horrordle.js
+++ horrordle/horrordle.js
@@ -7,25 +7,30 @@ let isGameOver = false;
 
 function loadGame() {
   // Fetching the dictionary
-  fetch('https://jonwcole.github.io/horrordle/dictionary.json')
+  fetch('https://jonwcole.github.io/horwordle/dictionary.json')
     .then(response => response.json())
     .then(data => {
       dictionary = data.map(word => word.toUpperCase());
     })
     .catch(error => console.error('Error loading dictionary:', error));
 
-// Fetching the word of the day
-const today = new Date().toISOString().slice(0, 10); // Get today's date in YYYY-MM-DD format
-fetch('https://jonwcole.github.io/horrordle/words.json')
-  .then(response => response.json())
-  .then(data => {
-    wordOfTheDay = data[today]?.toUpperCase(); // Access the word directly using the date key
-    if (!wordOfTheDay) {
-      console.error('Word for today not found');
-    }
-  })
-  .catch(error => console.error('Error loading word of the day:', error));
-};
+  // Adjusting for timezone offset
+  const now = new Date();
+  const timezoneOffset = now.getTimezoneOffset() * 60000; // Convert offset to milliseconds
+  const adjustedDate = new Date(now - timezoneOffset);
+  const today = adjustedDate.toISOString().slice(0, 10);
+
+  // Fetching the word of the day using the adjusted date
+  fetch('https://jonwcole.github.io/horwordle/words.json')
+    .then(response => response.json())
+    .then(data => {
+      wordOfTheDay = data[today]?.toUpperCase(); // Access the word directly using the date key
+      if (!wordOfTheDay) {
+        console.error('Word for today not found');
+      }
+    })
+    .catch(error => console.error('Error loading word of the day:', error));
+}
 
 // Handling virtual keyboard clicks
 document.getElementById('keyboard').addEventListener('click', function(e) {
