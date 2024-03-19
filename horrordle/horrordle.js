7d39892b1b4a3412bbe64fbdc25fc2cbe87718c5
jonwcole <jon@jonwcole.com>
2024-03-18 22:28:31 -0400

    Update horrordle.js

--- horrordle/horrordle.js
+++ horrordle/horrordle.js
@@ -216,7 +216,7 @@ function displayEndGameMessage(won) {
                 if (navButton) {
                     navButton.click();
                 }
-            }, 600); // Wait for fade out
+            }, 1200); // Wait for fade out
         }, 1200); // Duration message is shown
     }, 100); // Ensure display:block is applied
 }
