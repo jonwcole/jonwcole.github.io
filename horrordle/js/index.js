0db9d03484a9907428a4f3158ba894897155a3d6
jonwcole <jon@jonwcole.com>
2024-03-24 18:16:05 -0400

    Update index.js

--- horrordle/js/index.js
+++ horrordle/js/index.js
@@ -4,16 +4,15 @@ import { gameState } from './state/gameState.js';
 import { uiUpdater } from './ui/uiUpdater.js';
 import './ui/eventListeners.js';
 
-
+// Wait for the DOM to be fully loaded before initializing the game
 document.addEventListener('DOMContentLoaded', () => {
     initializeGame();
 });
 
-
+// Define the async function to initialize the game
 async function initializeGame() {
     await dataManager.loadDictionary();
     await dataManager.loadDailyWord(); // Only necessary if this is asynchronous and not yet called
     gameState.startNewGame(dataManager.dailyWord, dataManager.hint, dataManager.dictionary);
 }
 
-initializeGame();
