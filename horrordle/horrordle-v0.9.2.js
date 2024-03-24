148d0a1ca99f64c059c907fd5223dfe703e8c8bf
jonwcole <jon@jonwcole.com>
2024-03-23 22:26:41 -0400

    Update horrordle-v0.9.2.js

--- horrordle/horrordle-v0.9.2.js
+++ horrordle/horrordle-v0.9.2.js
@@ -586,10 +586,12 @@ document.getElementById('share-result').addEventListener('click', function() {
         .then(() => {
             // Display the copy confirmation instead of an alert
             document.getElementById('copy-confirmation').style.display = 'block';
+            document.getElementById('copy-confirmation').style.opacity = '1';
             
             // Optionally, you might want to hide the confirmation after a few seconds
             setTimeout(() => {
                 document.getElementById('copy-confirmation').style.display = 'none';
+                document.getElementById('copy-confirmation').style.opacity = '0';
             }, 3000); // Adjust time as needed
         })
         .catch(err => console.error('Failed to copy result to clipboard:', err));
