cd36d3ef55fbd47be70fc5203007c5d14fba443f
jonwcole <jon@jonwcole.com>
2024-03-24 03:50:06 -0400

    Update uiUpdater.js

--- horrordle/js/ui/uiUpdater.js
+++ horrordle/js/ui/uiUpdater.js
@@ -1,23 +1,23 @@
-function updateCurrentGuessDisplay(currentGuess) {
-    // Update the DOM elements representing the current guess
-    console.log(`Update display with current guess: ${currentGuess}`);
-}
-
-function markGuessResult(guess, result) {
-    // Update the tiles to show correct/present/absent
-    console.log(`Guess result: ${guess} - ${result}`);
-}
-
-function showEndGameMessage(won, word) {
-    if (won) {
-        console.log('Congratulations! You guessed the word:', word);
-    } else {
-        console.log('Game over. The word was:', word);
+const uiUpdater = {
+    updateCurrentGuessDisplay(currentGuess) {
+        // Update the DOM elements representing the current guess
+        console.log(`Update display with current guess: ${currentGuess}`);
+    },
+    markGuessResult(guess, result) {
+        // Update the tiles to show correct/present/absent
+        console.log(`Guess result: ${guess} - ${result}`);
+    },
+    function showEndGameMessage(won, word) {
+        if (won) {
+            console.log('Congratulations! You guessed the word:', word);
+        } else {
+            console.log('Game over. The word was:', word);
+        }
+    },
+    updateGuessDisplay(currentGuess) {
+        // Implementation...
+        console.log(`Update display with current guess: ${currentGuess}`);
     }
-    // Update the UI with the end game message
-}
+};
 
-function updateGuessDisplay(currentGuess) {
-    // Update the UI to reflect the current guess
-    // This could involve setting the text content of DOM elements representing guess tiles
-}
+export { uiUpdater };
