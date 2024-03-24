64c7e14f42108fa603107a36f95c955cc9ecc927
jonwcole <jon@jonwcole.com>
2024-03-24 19:01:58 -0400

    Update uiUpdater.js

--- horrordle/js/ui/uiUpdater.js
+++ horrordle/js/ui/uiUpdater.js
@@ -102,7 +102,6 @@ const uiUpdater = {
             }, 100);
 
             // Fill the hintElement with the hint of the day
-            hintElement.textContent = hint; // Use the passed hint parameter
             if (hintElement) {
                 hintElement.textContent = hint; // Update the hint text
             }
