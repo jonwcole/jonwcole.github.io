42c16aa79082c5ade15a98aef7ffdcae4c2340b1
jonwcole <jon@jonwcole.com>
2024-03-25 22:20:49 -0400

    Update gameState.js

--- horrordle/js/state/gameState.js
+++ horrordle/js/state/gameState.js
@@ -205,7 +205,7 @@ class GameState {
         localStorage.setItem('gameDate', today);
 
         const gameGuessLetters = this.guesses;
-        const gameGuessColors = this.guesses.map(guess => this.compareGuess(guess).map(result => result === 'correct' : result === 'present' : 'absent'));
+        const gameGuessColors = this.guesses.map(guess => this.compareGuess(guess).map(result => 'correct' : 'present' : 'absent'));
 
         localStorage.setItem('gameGuessLetters', JSON.stringify(gameGuessLetters));
         localStorage.setItem('gameGuessColors', JSON.stringify(gameGuessColors));
