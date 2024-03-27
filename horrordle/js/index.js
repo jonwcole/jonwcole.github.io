d436551124988645decad68076d62ae313f577f1
jonwcole <jon@jonwcole.com>
2024-03-27 01:40:44 -0400

    Update index.js

--- horrordle/js/index.js
+++ horrordle/js/index.js
@@ -5,24 +5,16 @@ import { uiUpdater } from './ui/uiUpdater.js';
 import './ui/eventListeners.js';
 import './logic/inputHandler.js';
 
-// Wait for the DOM to be fully loaded before initializing the game
-document.addEventListener('DOMContentLoaded', async () => {
-    await initializeGame(); // Ensure game is initialized before proceeding
-    gameState.restoreGameState();
-    uiUpdater.updateStatsDisplay(gameState.stats);
-    // If updateUI is designed to refresh the UI based on the gameState's current state, call it here after restoration and UI initialization
-    gameState.updateUI(); // Assuming this now internally uses the uiUpdater initialized earlier
-});
-
 async function initializeGame() {
     await dataManager.loadDictionary();
     await dataManager.loadDailyWord();
-    gameState.startNewGame(dataManager.dailyWord, dataManager.hint, dataManager.dictionary);
-    gameState.init(uiUpdater); // Configures the gameState with uiUpdater
-    uiUpdater.updateStatsDisplay(gameState.stats); // Update UI with the initial or restored game state
+    // Pass the daily word and hint directly from dataManager to gameState
+    gameState.loadGameDetails(dataManager.dailyWord, dataManager.hint, dataManager.dictionary);
 }
 
-
-
-
-
+// Wait for the DOM to be fully loaded before initializing the game
+document.addEventListener('DOMContentLoaded', async () => {
+    await initializeGame();
+    gameState.init(uiUpdater); // Configures the gameState with uiUpdater
+    uiUpdater.updateStatsDisplay(gameState.stats); // Update UI with the initial or restored game state
+});
