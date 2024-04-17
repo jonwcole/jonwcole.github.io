d47e912ea15b3ce6f304fc881a1842462e17e5b1
jonwcole <jon@jonwcole.com>
2024-04-16 20:08:43 -0400

    Update horrordle-v0.9.4.js

--- horrordle/horrordle-v0.9.4.js
+++ horrordle/horrordle-v0.9.4.js
@@ -217,7 +217,7 @@ function updateTiles(attempt, guess, result) {
                     tile.classList.add('tile-win-pop'); // Apply win animation class to the current tile
                 }, 500 * tiles.length + (500 * index)); // Start celebrations after all tiles are flipped, then stagger each
             }
-        }, index * 500); // Stagger the start of each tile's flip
+        }, index * 100); // Stagger the start of each tile's flip
     });
 
     updateKeyboard(guess, result);
