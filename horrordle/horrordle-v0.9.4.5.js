1a97cf334531d11850b2654681fc88a10907f832
jonwcole <jon@jonwcole.com>
2024-11-04 01:12:56 -0500

    Update horrordle-v0.9.4.5.js

--- horrordle/horrordle-v0.9.4.5.js
+++ horrordle/horrordle-v0.9.4.5.js
@@ -52,7 +52,7 @@ async function loadGame() {
 
             // Update the UI with the original word for display
             updateGameUI(wordOfTheDay, hintOfTheDay, contextOfTheDay);
-        } 
+        }
         if (!wordData) {
             // Display a message if the word for today is missing
             const errorMessage = document.getElementById('error-message');
